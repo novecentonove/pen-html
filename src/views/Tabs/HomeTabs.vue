56df0400b98857720b41d4f9459aed37a1182733
davide marchiori <davide.m@heveloon.co.uk>
2023-11-14 16:05:36 +0100

    clean

--- src/views/Tabs/HomeTabs.vue
+++ src/views/Tabs/HomeTabs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home_tabs">
+  <div class="home_tab_list">
     <ul class="inline-flex px-1 w-full border-b">
       <li v-for="(file, i) in openFiles" :key="i" @click="selectedPath = file.path"
         :class="{ 'border-b': selectedPath != file.path}" 
@@ -38,3 +38,33 @@
   })
 
 </script>
+
+<style>
+
+.list-enter-from{
+  opacity: 0;
+}
+.list-enter-to{
+  opacity: 1;
+}
+.list-enter-active{
+  transition: all 0.3s ease-out;
+  position: absolute
+}
+.list-leave-from{
+  opacity: 1;
+}
+.list-leave-to{
+  opacity: 0;
+}
+.list-leave-active{
+  transition: all 0.3s ease-out;
+  position: absolute /* DA USARE PER LIST MOVE */
+}
+
+/* spostamento da uno stato e l'altro*/
+.list-move{
+  transition: all 0.3s ease-out;
+}
+
+</style>
