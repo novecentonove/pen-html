6b37a86d3b84230bd7f4f8ecf90d1b1382349603
davide marchiori <davide.m@heveloon.co.uk>
2024-01-06 01:13:46 +0100

    fix css folder icon

--- src/components/FileDrawer/FileTree.vue
+++ src/components/FileDrawer/FileTree.vue
@@ -1,7 +1,7 @@
 <template>
 
   <li v-if="props.children" :style="indent" class="file_li" @click="toggleChildren()" @contextmenu.prevent="toggleMenu('dir')">
-    <FolderIcon :size="12" class="mr-1" /><span class="file_li_inner cut_text">{{ props.file.name }}</span>
+    <FolderIcon width="0.95em" class="mr-1" /><span class="file_li_inner cut_text">{{ props.file.name }}</span>
   </li>
 
   <li v-else ref="toggleMenuRef" :style="indent" class="file_li marker:relative" @contextmenu.prevent="toggleMenu()">
